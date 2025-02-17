f8c819334920a450e7b7a2733b123fc43d7e39fb
Saullo Bueno <saullobueno@gmail.com>
2025-02-17 20:29:23 +0000

    Add files via upload

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+// configurando para importamos sem precisar ficar colocando ../../ e agora usamos o til (~)
+
+const { addBabelPlugin, override } = require('customize-cra');
+
+module.exports = override(
+  addBabelPlugin([
+    'babel-plugin-root-import',
+    {
+      rootPathSuffix: 'src',
+    },
+  ])
+);
